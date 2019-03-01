f3e88a497176f182522ae0a725e74388b1319690
Dmitriy C <dimitrii9000@mail.ru>
2019-03-01 19:49:11 +0300

    another

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,11 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
+port        ENV.fetch("PORT") { PORT}
 
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "RAILS_ENV" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
